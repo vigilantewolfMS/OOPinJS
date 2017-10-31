9d57b0666a74a9b2606b064929d9f80220e1266f
vigilantewolfMS <32073374+vigilantewolfMS@users.noreply.github.com>
2017-10-31 13:39:44 -0400

    more stuff

--- sketch.js
+++ sketch.js
@@ -1,5 +1,28 @@
 let bubbles = [];
 
+
+function setup() { // built-in P5.JS function -=- this runs once
+	createCanvas(600, 400); 
+	for (let i = 0; i < 6 ; i++)
+	{ 
+		let x = random(width);
+		let y = random (height);
+		let r = random (10,40);
+	bubbles[i] = new Bubble (x, y, r);
+}
+}
+
+
+function draw() { // built-in P5.JS function -=-  automatic loop that repeats forever
+	background(0); // give the canvas a black background
+	for (let i = 0; i < 6 ; i++)
+	{
+		bubbles[0].move();
+		bubbles[0].display();
+	}
+	
+}
+
 class Bubble{
 	constructor(x, y, r,){
 		this.x = x;
@@ -15,19 +38,8 @@ class Bubble{
 	stroke(255); // white outline
 	strokeWeight(4); // line width
 	noFill();
-	ellipse(this.x, this.y, this.r * 2); // draw an ellipse/circle
+	ellipse(this.x, this.y, this.r ); // draw an ellipse/circle
 }
-}
-
-function setup() { // built-in P5.JS function -=- this runs once
-	createCanvas(600, 400); 
-	bubbles[0] = new Bubble (200, 200, 40);
-}
-
-function draw() { // built-in P5.JS function -=-  automatic loop that repeats forever
-	background(0); // give the canvas a black background
-	bubbles[0].move();
-	bubbles[0].display();
 }
 
 
