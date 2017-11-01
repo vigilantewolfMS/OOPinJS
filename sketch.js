6c03ded12d7e0bd905cc33a24ffaacc0d83ffa86
vigilantewolfMS <32073374+vigilantewolfMS@users.noreply.github.com>
2017-11-01 13:37:51 -0400

    bubble pressing fills bubble

--- sketch.js
+++ sketch.js
@@ -7,18 +7,22 @@ function setup() { // built-in P5.JS function -=- this runs once
 	{ 
 		let x = random(width);
 		let y = random (height);
-		let r = random (10,40);
+		let r = random (50,100);
 	bubbles[i] = new Bubble (x, y, r);
 }
 }
 
+function mousePressed(){
+	for (let i=0 ;  i < bubbles.length ; i++)
+		bubbles[i].clicked(mouseX, mouseY);
+}
 
 function draw() { // built-in P5.JS function -=-  automatic loop that repeats forever
 	background(0); // give the canvas a black background
-	for (let i = 0; i < 6 ; i++)
+	for (let i = 0; i < bubbles.length ; i++)
 	{
-		bubbles[0].move();
-		bubbles[0].display();
+		bubbles[i].move();
+		bubbles[i].display();
 	}
 	
 }
@@ -28,17 +32,27 @@ class Bubble{
 		this.x = x;
 		this.y= y;
 		this.r= r;
+		this.brightness = 0;
 
 	}
+	
+	clicked(px, py){
+		let d = dist(px, py, this.x, this.y);
+		if(d < this.r){
+			this.brightness = 255;
+		}
+	}
+	
 	move() {
 		this.x = this.x + random(-5,5);
 		this.y = this.y + random(-5,5);
         }
+		
 	display() {
 	stroke(255); // white outline
 	strokeWeight(4); // line width
-	noFill();
-	ellipse(this.x, this.y, this.r ); // draw an ellipse/circle
+	fill(this.brightness, 125);
+	ellipse(this.x, this.y, this.r, this.r ); // draw an ellipse/circle
 }
 }
 
